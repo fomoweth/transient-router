eecfeb43316311b53c7f9d0eedfd045a521124e8
Ryan <rkim@greenwood.app>
2024-07-05 15:03:11 -0700

    removed required functions from reverter library

--- src/libraries/Reverter.sol
+++ src/libraries/Reverter.sol
@@ -5,22 +5,6 @@ pragma solidity ^0.8.25;
 /// @notice Provides functions for reverting with a custom exception and value
 
 library Reverter {
-	function required(bool condition, bytes4 exception, address value) internal pure {
-		if (!condition) revertWith(exception, value);
-	}
-
-	function required(bool condition, bytes4 exception, bytes32 value) internal pure {
-		if (!condition) revertWith(exception, value);
-	}
-
-	function required(bool condition, bytes4 exception, uint256 value) internal pure {
-		if (!condition) revertWith(exception, value);
-	}
-
-	function required(bool condition, bytes4 exception) internal pure {
-		if (!condition) revertWith(exception);
-	}
-
 	function revertWith(bytes4 exception, address value) internal pure {
 		assembly ("memory-safe") {
 			mstore(0x00, exception)
