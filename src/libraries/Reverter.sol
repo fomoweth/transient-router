ea0cdbb82aad8b416ec33354e88c0e7c4e1fd08f
Ryan <rkim@greenwood.app>
2024-07-05 12:36:05 -0700

    added custom revert library

---
+++ src/libraries/Reverter.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+/// @title Reverter
+/// @notice Provides functions for reverting with a custom exception and value
+
+library Reverter {
+	function required(bool condition, bytes4 exception, address value) internal pure {
+		if (!condition) revertWith(exception, value);
+	}
+
+	function required(bool condition, bytes4 exception, bytes32 value) internal pure {
+		if (!condition) revertWith(exception, value);
+	}
+
+	function required(bool condition, bytes4 exception, uint256 value) internal pure {
+		if (!condition) revertWith(exception, value);
+	}
+
+	function required(bool condition, bytes4 exception) internal pure {
+		if (!condition) revertWith(exception);
+	}
+
+	function revertWith(bytes4 exception, address value) internal pure {
+		assembly ("memory-safe") {
+			mstore(0x00, exception)
+			mstore(0x04, value)
+			revert(0x1c, 0x24)
+		}
+	}
+
+	function revertWith(bytes4 exception, bytes32 value) internal pure {
+		assembly ("memory-safe") {
+			mstore(0x00, exception)
+			mstore(0x04, value)
+			revert(0x1c, 0x24)
+		}
+	}
+
+	function revertWith(bytes4 exception, uint256 value) internal pure {
+		assembly ("memory-safe") {
+			mstore(0x00, exception)
+			mstore(0x04, value)
+			revert(0x1c, 0x24)
+		}
+	}
+
+	function revertWith(bytes4 exception) internal pure {
+		assembly ("memory-safe") {
+			mstore(0x00, exception)
+			revert(0x1c, 0x04)
+		}
+	}
+}
